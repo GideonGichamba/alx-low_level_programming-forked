36e23ef07af57e1b1f36f862a03dd71cfd0c11a8
sharifhabiman <hdidiersharif@gmail.com>
2021-08-27 20:24:08 +0200

    positive nagative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,13 +10,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n>0){
-        printf("is positive");
-    }else if(n<0){
-        printf("is negative");
+	if(n<0){
+        printf("%i is negative");
+    }else if(n==0){
+        printf("%i is zero");
     }
-    else{
-        printf("is zero");
+    else if(n>0){
+        printf("%i is positive");
     }
 	return (0);
 }
