289ceee86532787b488f25486be9d07b2930ca89
sharifhabiman <hdidiersharif@gmail.com>
2021-08-31 12:51:20 +0200

    pritn Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,19 +2,23 @@
 
 /**
  * main entry point
+ * 
  * Description:Prints Holberton with _putchar
+ * 
  * retrun 0 for success
  */
-int main(void){
+
+int main(void)
+{
    int i=0;
    char c;
-   char s[]="Holberton\n";
+   char s[] = "Holberton\n";
+
    while (i<=9)
    {
-       c=s[i];
+       c = s[i];
        _putchar(c);
        i++;
    }
-   
     return(0);
 }
