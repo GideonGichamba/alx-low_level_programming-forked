47e0652babb3c6ee0d63486cab459a729a796d13
sharifhabiman <hdidiersharif@gmail.com>
2021-09-06 22:51:57 +0200

    reverse numbers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 /**
  * print_rev - reverse a string add newline
  * @s: string parameter
@@ -5,16 +6,15 @@
  */
 void print_rev(char *s)
 {
- int i=0;
- while (*(s + i))
- i++;
- i=i-1;
- while (i >= 0)
- {
-     putchar(*(s + i));
-     i--;
+int i = 0;
+while (*(s + i))
+i++;
+i = i - 1;
+while (i >= 0)
+{
+putchar(*(s + i));
+i--;
 
- }
-   putchar('\n');
-   
+}
+putchar('\n');
 }
