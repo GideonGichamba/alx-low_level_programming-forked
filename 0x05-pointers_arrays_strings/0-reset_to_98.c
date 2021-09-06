902cb48c7a96e0fca4b2db7042b17fae6d985736
sharifhabiman <hdidiersharif@gmail.com>
2021-09-06 21:54:29 +0200

    reset value of pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 /**
-* reset_to_98 - update the value to 98
-* @n :pointer to reset to 98 
-* return:nothing
-*/
+ * reset_to_98 - resets value of pointer to 98
+ * @n: pointer to reset to 98
+ *
+ * Return: void
+ */
 void reset_to_98(int *n)
 {
-*n = 98;
+	*n = 98;
 }
