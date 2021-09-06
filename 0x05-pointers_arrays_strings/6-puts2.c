e09cd3fb34b537e7a735ae18fd13008b9805a274
sharifhabiman <hdidiersharif@gmail.com>
2021-09-06 23:02:32 +0200

    reverse string 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include <stdio.h>
 
 /**
  * puts2 - prints every other character of a  string to stdout
@@ -8,12 +8,5 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
-
-	while (*(str + i))
-	{
-		_putchar(*(str + i));
-		i = i + 2;
-	}
-	_putchar('\n');
+	puts(str);
 }
