a74b2d5da185fd6036c1f53edc9e860d883672e3
sharifhabiman <hdidiersharif@gmail.com>
2021-09-08 11:06:17 +0200

    concatinate strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+/**
+ * _strcat - fuction to concatinate strings
+ * @dest: frist string
+ * @src: second string
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+char *conc = *dest + *src;
+return (*conc);
+}
