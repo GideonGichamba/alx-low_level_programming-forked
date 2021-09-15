a65fa9a37868f71a93661e759166a4f186e4263a
sharifhabiman <hdidiersharif@gmail.com>
2021-09-15 12:45:23 +0200

    Print string followed by new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * _puts_recursion - print function followed by newline
+ * @s: strinng to be printed
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+puts(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+# THis is recusive fuction project
