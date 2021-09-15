f74b461c5f80f30b53b1fc8eba9141250ece85de
sharifhabiman <hdidiersharif@gmail.com>
2021-09-15 18:50:38 +0200

    commit some changes

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,21 @@
 #include "main.h"
+/**
+ * wildcard - checks for the wildcards
+ * @s3: string to compare to
+ * @s4: string to check
+ *
+ * Return: pointer to spot in s3 that matches s4
+ */
+char *wildcard(char *s3, char *s4)
+{
+	if (*s3 != *s4)
+	{
+		if (*s3 == '\0')
+			return (0);
+		return (wildcard(s3 + 1, s4));
+	}
+	return (test(s3 + 1, s4 + 1, s3, s4));
+}
 
 /**
  * test - tests if wildcard is a proper match
@@ -18,23 +35,7 @@ char *test(char *s5, char *s6, char *t5, char *t6)
 	else
 		return (test(s5 + 1, s6 + 1, t5, t6));
 }
-/**
- * wildcard - checks for the wildcards
- * @s3: string to compare to
- * @s4: string to check
- *
- * Return: pointer to spot in s3 that matches s4
- */
-char *wildcard(char *s3, char *s4)
-{
-	if (*s3 != *s4)
-	{
-		if (*s3 == '\0')
-			return (0);
-		return (wildcard(s3 + 1, s4));
-	}
-	return (test(s3 + 1, s4 + 1, s3, s4));
-}
+
 
 /**
  * wildcmp - compares two strings
