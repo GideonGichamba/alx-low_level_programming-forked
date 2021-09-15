514c3fade9f73e0de7b56177b702b491bee0a59e
sharifhabiman <hdidiersharif@gmail.com>
2021-09-15 15:55:58 +0200

    power the number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+/**
+ * _pow_recursion - raise x to power of y
+ * @x: number to raise by
+ * @y: number to power to x
+ * Return: the power
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+return (0);
+return (x * _pow_recursion(x, y - 1));
+}
