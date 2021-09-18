95fedb7b199e911d45a21476ed16c89f3f1ba065
sharifhabiman <hdidiersharif@gmail.com>
2021-09-18 18:41:08 +0200

    Whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: return 1 0r zero
+ */
+int main(int argc, char **argv)
+{
+printf("%s", argv[0]);
+exit(EXIT_SUCCESS);
+}
