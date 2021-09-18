d032ddcc176d3cac89146f53ea778c1dcd9039b3
sharifhabiman <hdidiersharif@gmail.com>
2021-09-18 18:52:33 +0200

    Whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: array of arguments
  * Return: return 1 0r zero
  */
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
 printf("%s\n", argv[0]);
 exit(EXIT_SUCCESS);
