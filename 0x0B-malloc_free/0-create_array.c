ff858d1571cbd342df29b2a1f3786315030266e8
sharifhabiman <hdidiersharif@gmail.com>
2021-09-22 17:58:53 +0200

    creating an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,14 +3,14 @@
  * create_array- function to create an array
  * @size:size of an array
  * @c:characeter;
- * Return
+ * Return: allocacated array;
  */
 char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 char *a;
 a = malloc(size * sizeof(char));
-if (a == NULL)
+if (a == NULL || size == 0)
 return (NULL);
 for (i = 0; i < size; i++)
 *(a + i) = c;
