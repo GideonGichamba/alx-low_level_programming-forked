ba4095237dac697224d6d534abbe4b2763e9d0e2
sharifhabiman <hdidiersharif@gmail.com>
2022-03-29 10:17:38 +0200

    debuging

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,7 @@
 #ifndef _SEARCH_ALGOS_
 #define _SEARCH_ALGOS_
-#include <stdlib.h>;
-#include<stdio.h>;
+#include <stdlib.h>
+#include<stdio.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
